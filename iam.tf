b047926c68d3bb91a36f319a982e55ac1fa2f8d1
Sebastian Ramsland <SebRamsland@users.noreply.github.com>
2026-02-02 10:36:06 +0100

    chore: Use Skrastrek Terraform Registry (#48)

--- iam.tf
+++ iam.tf
@@ -6,7 +6,8 @@ resource "aws_iam_role" "this" {
 }
 
 module "assume_role_policy_document" {
-  source = "github.com/skrastrek/terraform-modules-aws-iam//policy-document/service-assume-role?ref=v0.2.0"
+  source  = "skrastrek/iam/aws//modules/policy-document/service-assume-role"
+  version = "1.0.0"
 
   service_identifiers = ["lambda.amazonaws.com", "edgelambda.amazonaws.com"]
 }
@@ -17,7 +18,8 @@ resource "aws_iam_role_policy_attachment" "aws_lambda_basic_execution_role" {
 }
 
 module "config_secrets_manager_secret_read" {
-  source = "github.com/skrastrek/terraform-modules-aws-iam//role-policy/secretsmanager-secret-read?ref=v0.2.0"
+  source  = "skrastrek/iam/aws//modules/role-policy/secretsmanager-secret-read"
+  version = "1.0.0"
 
   role_name   = aws_iam_role.this.name
   policy_name = "config-secret-read"
