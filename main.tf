0b588835e22018d4b1e31b077066ec475ef6e0d3
Sebastian Ramsland <SebRamsland@users.noreply.github.com>
2026-06-03 13:33:15 +0200

    fix: Remove usage of deprecated attribute (#55)

--- main.tf
+++ main.tf
@@ -63,7 +63,7 @@ resource "aws_lambda_permission" "invoke_from_cloudfront" {
 }
 
 resource "aws_cloudwatch_log_group" "this" {
-  name              = "/aws/lambda/${data.aws_region.current.id}.${var.name}"
+  name              = "/aws/lambda/${data.aws_region.current.region}.${var.name}"
   retention_in_days = var.cloudwatch_log_group_retention_in_days
   kms_key_id        = var.cloudwatch_log_group_kms_key_id
 
